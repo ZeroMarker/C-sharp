2a67700d06e8363b4aa72a0eb90981ce3b196fed
ZeroMarker <u78k4l67@163.com>
2023-05-30 02:07:43 +0800

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Student : Person
+    {
+        public Student(int Id, string Name, int Age) : base(Id, Name, Age)
+        {
+        }
+    }
+}
